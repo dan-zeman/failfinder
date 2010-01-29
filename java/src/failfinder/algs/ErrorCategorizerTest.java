bf9f588b163ad5efe6391e46d88b31e3bc2c825a
jon.h.clark <jon.h.clark@007692b2-0a86-11df-b317-0f3410dc57f6>
2010-01-29 17:16:11 +0000

    Error categorizer has a good number of unit tests and passes all of them now.

--- java/src/failfinder/algs/ErrorCategorizerTest.java
+++ java/src/failfinder/algs/ErrorCategorizerTest.java
@@ -1,10 +1,9 @@
 package failfinder.algs;
 
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import failfinder.algs.ErrorCategorizer.Tristate;
@@ -13,45 +12,97 @@ import failfinder.data.NBestList;
 
 public class ErrorCategorizerTest {
 
-	@Test
-	public void testHasReachabilityError() {
-		
-		ErrorCategorizer cat = new ErrorCategorizer();
-		
-		// inputs
-		String desired = "fuzzy wuzzy was a bear";
-		NBestList emptyForced = new NBestList(new ArrayList<Hypothesis>(0));
-		NBestList errorForced = makeNbest("fuzzy wuzznt was a bear");
-		
-		NBestList winForced = makeNbest("fuzzy wuzzy was a bear");
+	private ErrorCategorizer cat;
+	private String desired;
+	private NBestList empty;
+	private NBestList fuzzyWuzznt1;
+	private NBestList fuzzyWuz1;
+	private NBestList fuzzyWuzBest1;
+	private NBestList fuzzyWuzWorst1;
+	private NBestList fuzzyWuzWorst5;
+	private NBestList fuzzyWuzznt5;
 
-		Tristate emptyResult = cat.hasReachabilityError(desired, emptyForced);
-		Assert.assertSame(Tristate.TRUE, emptyResult);
-		
-		Tristate errorResult = cat.hasReachabilityError(desired, errorForced);
-		Assert.assertSame(Tristate.TRUE, errorResult);
-		
-		Tristate winResult = cat.hasReachabilityError(desired, winForced);
-		Assert.assertSame(Tristate.FALSE, winResult);
+	@Before
+	public void setup() {
+		cat = new ErrorCategorizer();
+		desired = "fuzzy wuzzy was a bear";
+		empty = new NBestList(new ArrayList<Hypothesis>(0));
+		fuzzyWuz1 = makeNbest("fuzzy wuzzy was a bear", -1.0f);
+		fuzzyWuzznt1 = makeNbest("fuzzy wuzznt was a bear", -1.0f);
+		fuzzyWuzznt5 = makeNbest("fuzzy wuzznt was a bear", -5.0f);
+
+		fuzzyWuzBest1 = makeNbest("fuzzy wuzzy was a bear", -1.0f);
+		fuzzyWuzBest1.nbest.add(makeHyp("fuzzy wuzzy was not a bear", -2.0f));
+
+		fuzzyWuzWorst1 = makeNbest("fuzzy wuzzy was not a bear", -1.0f);
+		fuzzyWuzWorst1.nbest.add(makeHyp("fuzzy wuzzy was a bear", -2.0f));
+
+		fuzzyWuzWorst5 = makeNbest("fuzzy wuzzy was not a bear", -5.0f);
+		fuzzyWuzWorst5.nbest.add(makeHyp("fuzzy wuzzy was a bear", -6.0f));
 	}
 
-	private NBestList makeNbest(String str) {
-		ArrayList<Hypothesis> winList = new ArrayList<Hypothesis>(1);
-		Hypothesis hyp = new Hypothesis();
-		hyp.yield = str;
-		winList.add(hyp);
-		NBestList winForced = new NBestList(winList);
-		return winForced;
+	@Test
+	public void testHasReachabilityError() {
+		Tristate emptyResult = cat.hasReachabilityError(desired, empty);
+		Assert.assertSame(Tristate.TRUE, emptyResult);
+
+		Tristate errorResult = cat.hasReachabilityError(desired, fuzzyWuzznt1);
+		Assert.assertSame(Tristate.TRUE, errorResult);
+
+		Tristate winResult = cat.hasReachabilityError(desired, fuzzyWuz1);
+		Assert.assertSame(Tristate.FALSE, winResult);
 	}
 
 	@Test
 	public void testHasSearchError() {
-		fail("Not yet implemented");
+		Tristate emptyResult = cat.hasSearchError(desired, empty, fuzzyWuz1);
+		Assert.assertSame(Tristate.UNKNOWN, emptyResult);
+
+		Tristate errorResult = cat.hasSearchError(desired, fuzzyWuz1, fuzzyWuzznt1);
+		Assert.assertSame(Tristate.TRUE, errorResult);
+
+		Tristate winResult = cat.hasSearchError(desired, fuzzyWuz1, fuzzyWuz1);
+		Assert.assertSame(Tristate.FALSE, winResult);
 	}
 
 	@Test
 	public void testHasModelError() {
-		fail("Not yet implemented");
+
+		// reachability error
+		Tristate emptyResult = cat.hasModelError(desired, empty, fuzzyWuz1);
+		Assert.assertSame(Tristate.UNKNOWN, emptyResult);
+		
+		// fuzzy in forced
+		// but ties with wuzznt in vanilla
+		Tristate tieWithOther = cat.hasModelError(desired, fuzzyWuzBest1, fuzzyWuzznt1);
+		Assert.assertSame(Tristate.TRUE, tieWithOther);
+		
+		// fuzzy ties with itself in forced and vanilla
+		Tristate bestInVanilla = cat.hasModelError(desired, fuzzyWuzBest1, fuzzyWuzBest1);
+		Assert.assertSame(Tristate.FALSE, bestInVanilla);
+
+		// fuzzy best in forced but worst in vanilla
+		Tristate bestInForced = cat.hasModelError(desired, fuzzyWuzBest1, fuzzyWuzWorst5);
+		Assert.assertSame(Tristate.FALSE, bestInForced);
+		
+		// fuzzy was the best in forced, but was not found in vanilla
+		// because we diagnose with regard to the limited set of hypotheses
+		// we will call this no model error conditioned on ranking the nbest + forced
+		Tristate bestInForcedOnly = cat.hasModelError(desired, fuzzyWuzBest1, fuzzyWuzznt5);
+		Assert.assertSame(Tristate.FALSE, bestInForcedOnly);
 	}
 
+	private NBestList makeNbest(String str, float d) {
+		ArrayList<Hypothesis> winList = new ArrayList<Hypothesis>(1);
+		winList.add(makeHyp(str, d));
+		NBestList winForced = new NBestList(winList);
+		return winForced;
+	}
+
+	private Hypothesis makeHyp(String str, float d) {
+		Hypothesis hyp = new Hypothesis();
+		hyp.yield = str;
+		hyp.total = d;
+		return hyp;
+	}
 }
--- java/src/failfinder/data/NBestList.java
+++ java/src/failfinder/data/NBestList.java
@@ -23,10 +23,11 @@ public class NBestList {
 			return Float.NEGATIVE_INFINITY;
 		} else {
 			float topbestScore = nbest.get(0).total;
-			if(nbest.size() > 1) {
+			if (nbest.size() > 1) {
 				float secondScore = nbest.get(1).total;
-				if(topbestScore < secondScore) {
-					throw new RuntimeException("second-best score cannot be greater than topbest score. I thought we were using log probs...");
+				if (topbestScore < secondScore) {
+					throw new RuntimeException(
+							"second-best score cannot be greater than topbest score. I thought we were using log probs...");
 				}
 			}
 			return topbestScore;
@@ -35,11 +36,19 @@ public class NBestList {
 
 	public float getBestScoreFor(String desired) {
 		// TODO: hash
-		for(Hypothesis hyp : nbest) {
-			if(hyp.equals(desired)) {
+		for (Hypothesis hyp : nbest) {
+			if (hyp.yield.equals(desired)) {
 				return hyp.total;
 			}
 		}
 		return Float.NEGATIVE_INFINITY;
+	}
+
+	public Hypothesis getBest() {
+		if (nbest.size() > 0) {
+			return nbest.get(0);
+		} else {
+			return null;
+		}
 	}
 }
