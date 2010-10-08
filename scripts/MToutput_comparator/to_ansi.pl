65c9d120e7823699b269df2c5761bfaae842af36
ondrej.bojar <ondrej.bojar@007692b2-0a86-11df-b317-0f3410dc57f6>
2010-10-08 20:52:33 +0000

    hilight also unigrams that a system has produced completely alone

--- scripts/MToutput_comparator/to_ansi.pl
+++ scripts/MToutput_comparator/to_ansi.pl
@@ -8,13 +8,14 @@ use Term::ANSIColor;
 binmode STDIN, ":utf8";
 binmode STDOUT, ":utf8";
 
-my $color_tst1 = "yellow";
-my $color_tst2 = "cyan";
-my $color_tstboth = "green";
+my $color_tst1 = "yellow"; # confirmed by ref, in tst1 only
+my $color_tst2 = "cyan";  # confirmed by ref, in tst2 only
+my $color_tstboth = "green"; # confirmed by ref, in both tsts
+my $color_alone = "red";  # not confirmed, not seen in other tst
 # there is a bug in ANSICOLOR that swaps on_cyan and on_yellow, swap back
 my $oncolor_tst1 = "$color_tst2";
 my $oncolor_tst2 = "$color_tst1";
-my @COLORS = ("black on_$oncolor_tst1", "black on_$oncolor_tst2", "black on_$color_tstboth");
+my @COLORS = ("black on_$oncolor_tst1", "black on_$oncolor_tst2", "black on_$color_tstboth", "black on_$color_alone");
 
 my $reset = color("reset");
 
@@ -37,11 +38,12 @@ sub print_sentence {
     my $output;
 
     # separate brackets
-    $sentence =~ s/(\[\[\[|\]\]\]|\{\{\{|\}\}\})/\t$1\t/g;
+    $sentence =~ s/(\[\[\[|\]\]\]|\{\{\{|\}\}\}|<<<|>>>)/\t$1\t/g;
     $sentence =~ s/\t+/\t/g;
     $sentence =~ s/^\t*(.+)\t*$/$1/;
 
     my @chunks = split ( /\t/, $sentence );
+    my $in_angle = 0;
     my $in_square = 0;
     my $in_curly = 0;
     my $state = 0;
@@ -59,8 +61,17 @@ sub print_sentence {
         elsif ( $chunk eq '}}}' ) {
             $in_curly = 0;
         }
+        elsif ( $chunk eq '<<<' ) {
+            $in_angle = 1;
+        }
+        elsif ( $chunk eq '>>>' ) {
+            $in_angle = 0;
+        }
         else {
             my $new_state = $in_square * 2 + $in_curly;
+              # 0 for none, 1 for curly only, 2 for square only, 3 for both
+            $new_state = 4 if $new_state == 0 && $in_angle;
+              # 4 for alone
             if ( $new_state != $state ) {
                 $output .= $reset if $state != 0;
                 $output .= color($COLORS[$new_state - 1]) if $new_state != 0;
